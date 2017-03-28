430612915969f7d65a7c88bf4835bc2ab782f901
EduPH <epaluzohidalgo@gmail.com>
2017-03-28 17:58:46 +0200

    Update Pascal.hs

--- Pascal/Pascal.hs
+++ Pascal/Pascal.hs
@@ -1,11 +1,3 @@
--- readln(N);
--- counter := 0;
--- for i := N - 1 downto 1 do begin
---   counter := counter + 1;
---   if N mod i = 0 then break;
--- end;
--- writeln(counter);
-
 import Primes
 main :: IO ()
 main = interact solucion
